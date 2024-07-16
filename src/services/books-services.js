6abd9b56ff921597f7e100e7f959bfaa599b51d3
Avocado955 <mellis955@gmail.com>
2024-07-16 15:53:54 +1000

    adds more styling to the book card and removes all the styling to the main app from the vite

---
+++ src/services/books-services.js
@@ -0,0 +1,11 @@
+// https://www.googleapis.com/books/v1/volumes?q=
+// intitle:
+// inauthor:
+
+/**
+ * @description - Searchs Google Books API using the given search terms
+ * @param generalTerm - The general search term if wanting anything with the text included, not specific to author or title
+ * @param author - Searchs authors with the provided text
+ * @param title - Searches titles with the provided text
+ */
+export const getBooksBySearch = async (generalTerm, author, title) => {};
