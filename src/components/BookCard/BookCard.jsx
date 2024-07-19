6c363734995002af43456e6e7f68c6f8e4f7c733
Avocado955 <mellis955@gmail.com>
2024-07-19 15:58:02 +1000

    adds styling to the index

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import styles from "./BookCard.module.scss";
 import { ModalContext } from "../../context/ModalContextProvider";
+import { BookContext } from "../../context/BookContextProvider";
 
 const BookCard = ({ book }) => {
   const { setIsShowingModal } = useContext(ModalContext);
+  const { setBook } = useContext(BookContext);
   const onBookClicked = () => {
     console.log("This book was clicked " + book.volumeInfo.title);
+    setBook(book);
     setIsShowingModal(true);
   };
 
--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,14 +1,16 @@
 import { useContext, useState } from "react";
 import { ModalContext } from "../../context/ModalContextProvider";
 import styles from "./ModalWindow.module.scss";
+import { BookContext } from "../../context/BookContextProvider";
 
 const ModalWindow = ({ onClose }) => {
   const { isShowingModal, setIsShowingModal } = useContext(ModalContext);
+  const { book } = useContext(BookContext);
 
   return (
     <div className={styles.modal}>
       <h1>Modal Window</h1>
-      {/* <h2>{book.volumeInfo.title}</h2> */}
+      <h2>{book.volumeInfo.title}</h2>
       <button onClick={onClose}>Close</button>
     </div>
   );
