a0c55240f9e092784c4e200e728d8be9dd2e4975
Avocado955 <Mellis955@gmail.com>
2024-07-20 22:35:31 +1000

    sets the modal window in the middle, adds an overlay which if clicked closes the modal window

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ModalContext } from "../../context/ModalContextProvider";
 import styles from "./ModalWindow.module.scss";
 import { BookContext } from "../../context/BookContextProvider";
@@ -7,11 +7,30 @@ const ModalWindow = ({ onClose }) => {
   const { isShowingModal, setIsShowingModal } = useContext(ModalContext);
   const { book } = useContext(BookContext);
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+        document.body.style.overflow = "scroll"
+    }
+  }, [])
+
   return (
+    <div>
+    <div className={styles.overlay} onClick={onClose}>
+    </div>
     <div className={styles.modal}>
-      <h1>Modal Window</h1>
+      <div className={styles.modal_container}>
+      <div>
+        <img src={book.volumeInfo.imageLinks.thumbnail} alt="" />
+      </div>
+      <div>
       <h2>{book.volumeInfo.title}</h2>
-      <button onClick={onClose}>Close</button>
+      <h3>{book.volumeInfo.authors?.join(" & ") ?? "Author Information Unavailable"}</h3>
+      <p>{book.volumeInfo.description ?? "No Description Available"}</p>
+      </div>
+      </div>
+      <button onClick={onClose} className={styles.btn}>Close</button>
+    </div>
     </div>
   );
 };
