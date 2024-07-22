fd1ab4bc139143e623c0d841d15fa2f54789b6b1
Avocado955 <Mellis955@gmail.com>
2024-07-22 21:39:33 +1000

    adds some more styling

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import styles from "./SearchForm.module.scss";
 
 const SearchForm = ({ onSubmit }) => {
   const formRef = useRef(null);
@@ -13,14 +14,15 @@ const SearchForm = ({ onSubmit }) => {
 
   return (
     <form onSubmit={handleSubmit} ref={formRef}>
-      <div>
+      <div className={styles.searchBar}>
         <input
           type="text"
           name="generalTerm"
           id="generalTerm"
           placeholder="Enter Search Term Here"
+          className={styles.searchBar_input}
         />
-        <button type="submit">Search</button>
+        <button type="submit" className={styles.searchBar_btn}>Search</button>
       </div>
     </form>
   );
